ab463426f6ea52ac7da3c47aecc3692fed480b16
Mustafa Enes Güneruz <mustafaguneruz19@gmail.com>
2021-07-21 15:29:59 +0300

    test

--- Film Project - ES6/project.js
+++ Film Project - ES6/project.js
@@ -3,61 +3,42 @@ const titleElement = document.querySelector("#title")
 const directorElement = document.querySelector("#director")
 const urlElement = document.querySelector("#url")
 const secondCardBody = document.querySelectorAll(".card-body")[1]
-const clearFilms =  document.getElementById("clear-films")
 
-// Tüm eventleri yükleme
+// Load all events
 eventListeners()
 
 function eventListeners() {
-    form.addEventListener("submit",addFilm) 
-    document.addEventListener("DOMContentLoaded",function(){
-        let films = Storage.getFilmsFromStorage()
-        UI.loadAllFilms(films)
-    })
+    form.addEventListener("submit",addFilm)
+
+
 
     secondCardBody.addEventListener("click",deleteFilm)
-    clearFilms.addEventListener("click",clearAllFilms)
 }
 
 function addFilm(event) {
     const title = titleElement.value
-    const director = directorElement.value 
-    const url = urlElement.value 
+    const director = directorElement.value
+    const url = urlElement.value
 
-    if(title === "" || director === "" || url === "")
-    {
-        // Hata mesajı
-        UI.displayMessages("Tüm alanları doldurun!","danger")
-        
+    if (title === "" || director === "" || url === "") {
+        // Information message
     }
-    else{
-        const newFilm = new Film(title,director,url) // Yeni film 
+    else {
+        const newFilm = new Film(title, director, url)
 
-        UI.addFilmToUI(newFilm) // Arayüze film ekleme
-        Storage.addFilmToStorage(newFilm) // Storage'a film ekleme
-        UI.displayMessages("Film başarıyla eklendi!", "success")
+        UI.addFilmToUI(newFilm)
     }
 
-    UI.clearInputs(titleElement,urlElement,directorElement)
+    UI.clearInputs(titleElement,directorElement,urlElement)
 
-    event.preventDefault() // Submit eventinin default özelliklerini yoksay(amaç:sayfanın yenilenmesini engelleme)
+    event.preventDefault()
 }
 
 function deleteFilm(event) {
     if(event.target.id === "delete-film"){
         UI.deleteFilmFromUI(event.target)
-        Storage.deleteFilmFromStorage(event.target.parentElement.previousElementSibling
-            .previousElementSibling.textContent)
-
-        UI.displayMessages("Silme işlemi başarılı!","success")
     }
 }
 
-function clearAllFilms() {
 
-    if(confirm("Emin misiniz?")){
-        UI.clearAllFilmsFromUI()
-        Storage.clearAllFilmsFromStorage()
-    }
-    
-}
+
